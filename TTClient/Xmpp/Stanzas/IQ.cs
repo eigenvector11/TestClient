bbdf7a4b3d07ffdf1f6f0b53878d993dcbd5a279
Eigen Vector <eigenvector11@gmail.com>
2012-10-18 19:41:09 +0530

    Added the notion of an IQ stanza

---
+++ TTClient/Xmpp/Stanzas/IQ.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Utilities;
+
+namespace Xmpp.Stanzas
+{
+    public enum IQType
+    {
+        Error,
+        Get,
+        Set,
+        Result,
+        Query
+    }
+
+    public class IQ : Stanza
+    {
+        private static readonly string _NAME = "iq";
+
+        private static int _id = new Random().Next(100000, 1000000);
+
+        private static Dictionary<IQType, string> _enumDescriptionMap;
+
+        #region Constructors
+        
+        public IQ() 
+            : base(_NAME)
+        {
+            SetAttribute("id", GetNextId() + "");
+        }
+
+        public IQ(IQType type)
+            : base(_NAME)
+        {
+            SetAttribute("id", GetNextId() + "");
+            Type = (GetIqType(type));
+        }
+
+        public IQ(IQType type, string xmlns) 
+            : base(_NAME, xmlns)
+        {
+            SetAttribute("id", GetNextId() + "");
+            Type = (GetIqType(type));
+        }
+
+        public IQ(IQType type, String from, String to) 
+            : base(_NAME)
+        {
+            SetAttribute("id", GetNextId() + "");
+            Type = (GetIqType(type));
+            From = from;
+            To = to;
+        }
+        
+        #endregion
+
+        public static string GetNextId()
+        {
+            var id = ++_id;
+            return id.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public IQType GetIqType()
+        {
+            return GetIqType(GetAttribute(_TYPE));
+        }
+
+        public static bool IsSet(Packet iq)
+        {
+            return iq.HasAttribute(_TYPE, "set");
+        }
+
+        public static bool IsSuccess(Packet iq)
+        {
+            return iq.HasAttribute(_TYPE, "result");
+        }
+
+        public void AddSession(String xmlns)
+        {
+            AddChild("session", xmlns);
+        }
+
+        public void AddQuery(String xmlns)
+        {
+            AddChild("query", xmlns);
+        }
+
+        public IQ WithQuery(string xmlns)
+        {
+            AddQuery(xmlns);
+            return this;
+        }
+
+
+        private static IQType GetIqType(string type)
+        {
+            var map = GetDescriptionMap();
+            return map.Keys.FirstOrDefault(key => map[key] == type);
+        }
+
+        private static string GetIqType(IQType type)
+        {
+            return GetDescriptionMap()[type];
+        }
+
+        private static Dictionary<IQType, string> GetDescriptionMap()
+        {
+            return _enumDescriptionMap ??
+                        (_enumDescriptionMap = new Dictionary<IQType, string>
+                                                                     {
+                                                                         {IQType.Error, "error"},
+                                                                         {IQType.Get, "get"},
+                                                                         {IQType.Set, "set"},
+                                                                         {IQType.Result, "result"},
+                                                                         {IQType.Query, "query"}
+                                                                     });
+        }
+
+    }
+}
---
+++ TTClient/Xmpp/Stanzas/Stanza.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.Serialization;
+using Utilities;
+
+namespace Xmpp.Stanzas
+{
+    [DataContract]
+    public class Stanza : Packet
+    {
+        protected static readonly string _TYPE = "type";
+        protected static readonly string _FROM = "from";
+        protected static readonly string _ID = "id";
+        protected static readonly string _TO = "to";
+
+        
+        public Stanza(string name) : base(name, "jabber:client")
+        {}
+
+        public Stanza(string name, string xmlns) : base(name, xmlns)
+        {}
+
+        [DataMember]
+        public string From
+        {
+            get { return HasAttribute(_FROM) ? GetAttribute(_FROM) : ""; }
+            set { SetAttribute(_FROM, value); }
+        }
+
+        [DataMember]
+        public string To
+        {
+            get { return HasAttribute(_TO) ? GetAttribute(_TO) : ""; }
+            set { SetAttribute(_TO, value); }
+        }
+
+        [DataMember]
+        public string Id
+        {
+            get { return HasAttribute(_ID) ? GetAttribute(_ID) : ""; }
+            set { SetAttribute(_ID, value); }
+        }
+
+        [DataMember]
+        public string Type
+        {
+            get { return HasAttribute(_TYPE) ? GetAttribute(_TYPE) : ""; }
+            set { SetAttribute(_TYPE, value); }
+        }
+
+        [DataMember]
+        public string FromBare
+        {
+            get { return RemoveResource(From); }
+        }
+
+        [DataMember]
+        public string ToBare
+        {
+            get { return RemoveResource(To); }
+        }
+
+        public static string RemoveResource(string jid)
+        {
+            return jid.IndexOf("/", System.StringComparison.Ordinal) != -1 
+                ? jid.Substring(0, jid.IndexOf("/", System.StringComparison.Ordinal)) 
+                : jid;
+        }
+    }
+}
